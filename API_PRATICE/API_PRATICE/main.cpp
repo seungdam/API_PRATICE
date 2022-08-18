fa306ab6cef63e13e00b2c8f0144a8e3971c79fb
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-08-18 23:27:39 +0900

    [윈도우즈 API 정복 1 - 2] *윈도우창 생성하기*
    WndClass 구조체 초기화

--- API_PRATICE/API_PRATICE/main.cpp
+++ API_PRATICE/API_PRATICE/main.cpp
@@ -9,6 +9,18 @@ LPCTSTR lpszClass = TEXT("KeyBoard_Action"); // 클래스 명을 저장하기 위한 문자열
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPCSTR lpszCmdParam, int nCmdShow) {
 
+	// 윈도우즈의 구체적인 속성을 나타내는 윈도우 클래스 구조체를 초기화.
+	WNDCLASS wndClass;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.lpszClassName = lpszClass;
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hInstance = hInstance;
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndClass.lpszMenuName = NULL;
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
