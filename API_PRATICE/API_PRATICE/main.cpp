b0f80c9f36096b7bde51167c7cae708890cf7ae9
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-08-18 23:45:36 +0900

    [윈도우즈 API 정복]  *오류 수정* WinMain 인자와 wndProc 선언 부분 맞게 수정

--- API_PRATICE/API_PRATICE/main.cpp
+++ API_PRATICE/API_PRATICE/main.cpp
@@ -2,12 +2,12 @@
 #include <windows.h>
 
 // ****운영체제가 메세지 발생 시 호출하는 함수 (callback 함수)
-LRESULT CALLBACK WinProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 HINSTANCE g_hInst; // 응용프로그램 핸들을 저장하기 위해 전역변수로 선언
 LPCTSTR lpszClass = TEXT("KeyBoard_Action"); // 클래스 명을 저장하기 위한 문자열 
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPCSTR lpszCmdParam, int nCmdShow) {
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
 
 	// 윈도우즈의 구체적인 속성을 나타내는 윈도우 클래스 구조체를 초기화.
 	WNDCLASS wndClass;
