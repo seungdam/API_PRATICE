a0cb67555a0c62249d31e7a611d09a3ea0c2d6b2
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-08-21 21:35:21 +0900

    [윈도우즈 API 정복 2 - 3] *리소스 실습(Menu)*
    각 메뉴에 대한 accelerator를 적용

--- API_PRATICE/API_PRATICE/resource.h
+++ API_PRATICE/API_PRATICE/resource.h
@@ -3,6 +3,7 @@
 // Resource.rc에서 사용되고 있습니다.
 //
 #define IDR_MENU1                       101
+#define IDR_ACCELERATOR1                102
 #define ID_MENU_MENU1                   40001
 #define ID_MENU_MENU2                   40002
 #define ID_MENU_EXIT                    40003
@@ -11,8 +12,8 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40008
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
