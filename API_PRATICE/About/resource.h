8f9f5aac955c598089d85328db7642ccef0a1b9c
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-08-24 23:41:42 +0900

    [윈도우즈 API 정복 4 - 16] *다이얼로그 실습(studentInfo)*
    다이얼로그 리소스 생성

--- API_PRATICE/About/resource.h
+++ API_PRATICE/About/resource.h
@@ -4,20 +4,30 @@
 //
 #define IDD_DIALOG1                     101
 #define IDD_DIALOG2                     103
+#define IDD_DIALOG3                     105
 #define IDC_EDIT1                       1003
 #define IDC_X                           1003
+#define IDC_ID                          1003
 #define IDC_EDIT2                       1004
 #define IDC_Y                           1004
+#define IDC_PW                          1004
 #define IDC_EDIT3                       1005
 #define IDC_STR                         1005
+#define IDC_M                           1006
+#define IDC_GE                          1007
+#define IDC_FM                          1008
+#define IDC_DS                          1009
+#define IDC_CE                          1010
+#define IDC_RADIO4                      1013
+#define IDC_ET                          1013
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
