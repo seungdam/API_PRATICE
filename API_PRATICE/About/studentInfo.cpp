be8dca5259fe5393d17b15304834736b08815366
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-08-24 23:55:22 +0900

    [윈도우즈 API 정복 4 - 19] *다이얼로그 실습(studentInfo)*
    다이얼로그 출력 및 초기화

--- API_PRATICE/About/studentInfo.cpp
+++ API_PRATICE/About/studentInfo.cpp
@@ -15,13 +15,45 @@ struct STDINFO {
 };
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
-	DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)MainDlgProc); // 윈도우 생성 없이 바로 다이얼로그 출력
-	return 0;
+	HWND hWnd;   // 윈도우 핸들
+	MSG Message; // 메세지
+	g_hInst = hInstance; // 인스턴스 핸들을 다른 프로세스에서도 사용할 수 있도록 전역변수에 대입
+
+	// 윈도우 클래스(윈도우의 속성을 정의하는 구조체)를 초기화
+	WNDCLASS wc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = (HBRUSH)(COLOR_BTNFACE + 1);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hInstance = hInstance;
+	wc.lpszClassName = lpszClass;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszMenuName = NULL;
+	wc.style = CS_VREDRAW | CS_HREDRAW;
+	RegisterClass(&wc); // 초기화 시킨 윈도우 클래스를 운영체제에 등록
+
+	// 윈도우 클래스 바탕으로 윈도우창을 생성한다
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT
+		, NULL, NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow); // 윈도우창을 출력
+	if (hWnd == NULL) return -1;
+
+	// 메세지 루프
+	while (GetMessage(&Message, NULL, 0, 0) > 0) { // 메세지 큐에 있는 메세지들을 확인
+		TranslateMessage(&Message); // 키보드에 입력된 메세지를 인스턴스가 이해하기 쉬운 형태로 해석
+		DispatchMessage(&Message); // WndProc으로 해석시킨 메세지를 전달한다.
+	}
+
+	return (int)Message.wParam;
 }
 
 BOOL CALLBACK MainDlgProc(HWND hDlg, UINT iMessage, WPARAM wParam, LPARAM lParam) {
 	switch (iMessage) {
 	case WM_INITDIALOG: // 다이얼로그 초기화 부분
+		CheckDlgButton(hDlg, IDC_M, TRUE);
+		CheckRadioButton(hDlg, IDC_GE,IDC_ET,IDC_GE);
 		return TRUE;
 	case WM_COMMAND:
 		switch (LOWORD(wParam)) {  // 컨트롤에 대한 처리
@@ -42,6 +74,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 	PAINTSTRUCT ps;
 	switch (iMessage) {
 	case WM_CREATE:
+		break;
+	case WM_RBUTTONDOWN:
+		DialogBox(g_hInst, MAKEINTRESOURCE(IDD_DIALOG3), hWnd, (DLGPROC)MainDlgProc);
 		break;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
